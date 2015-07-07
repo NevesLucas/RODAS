297694045e5c9dc40298925d1a696bcc5b9fb378
NevesLucas <lneves@bu.edu>
2015-07-07 18:10:21 -0400

    implemented point cloud translation function

--- PCL_test/PCTranslate.h
+++ PCL_test/PCTranslate.h
@@ -1,32 +1,23 @@
 #include <iostream>
-
-#include <pcl/io/pcd_io.h>
-#include <pcl/io/ply_io.h>
 #include <pcl/point_cloud.h>
-#include <pcl/console/parse.h>
 #include <pcl/common/transforms.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
+
 using namespace std;
-pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+void translate(float x, float y, float z, float angz, pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr  transformed_cloud)
+{
+	float theta = angz; // TODO: convert angz to radians
 
+	Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
 
-/*  METHOD #2: Using a Affine3f
-This method is easier and less error prone
-*/
-Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+	// Define a translation .
+	transform_2.translation() << x, y, z;
 
-// Define a translation of 2.5 meters on the x axis.
-transform_2.translation() << 2.5, 0.0, 0.0;
+	//  tetha radians arround Z axis
+	transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
 
-// The same rotation matrix as before; tetha radians arround Z axis
-transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
+	// apply transform_2
+	pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2);
 
-// Print the transformation
-printf("\nMethod #2: using an Affine3f\n");
-std::cout << transform_2.matrix() << std::endl;
-
-// Executing the transformation
-pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-// You can either apply transform_1 or transform_2; they are the same
-pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2
+}
